bb6e4ac7b3fe871fcb5313bed94b3f8cf41dbf07
Roni Seppälä <roni.o.seppala@gmail.com>
2025-07-27 19:23:28 +0300

    made my-cat.c

--- documentation/project_summary_and_advancment.md
+++ documentation/project_summary_and_advancment.md
@@ -5,3 +5,15 @@ The project was started with creating a README.md and documentaion folder
 After this the repository was pubilshed to GitHub
 
 After this i opened my c development environment of choice, which was WSL, created the placeholder c files.
+
+## Utility creation
+After the initial setup, I started creating the utilities one by one, starging from my-cat.c
+
+
+# Utility demonstrations
+## my-cat
+demo
+## my-grep
+demo
+## my-zip and my-unzip
+demo
--- my-cat.c
+++ my-cat.c
@@ -1,7 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        return 0; // No arguments provided, do nothing
+    }
 
-int main(void) {
-    // This is a simple program that does nothing
+    for (int i = 1; i < argc; i++) {
+        FILE *file = fopen(argv[i], "r");
+        if (file == NULL) { //check if file opened successfully
+            printf("my-cat: cannot open file\n");
+            return 1;
+        }
+
+        // Read and print the contents of the file
+        int ch;
+        int last = -1;
+        while ((ch = fgetc(file)) != EOF) {
+            putchar(ch);
+            last = ch;
+        }
+        fclose(file);
+        if (last != '\n' && last != -1) {
+            putchar('\n');
+        }
+    }
+
     return 0;
 }
-// It is intended to be a placeholder for future functionality
